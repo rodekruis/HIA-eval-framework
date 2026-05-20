b29284c59c8b19aeadf2709a02a220f662e6117e
dariavladutu236578 <dvladutu@rodekruis.nl>
2026-05-20 02:28:59 +0200

    metric validation

--- data_generation/main.py
+++ data_generation/main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 print("Starting data generation...")
 # LOADING INPUT QUESTIONS
-data = pd.read_csv(r"../synthetic_data/goldens.csv")
+data = pd.read_csv("../data/goldens.csv")
 input_questions = data['input']
 
 # CONNECTION TO CHATBOT ENDPOINT
@@ -18,7 +18,7 @@ key = os.getenv('chatbot_key')
 # making the HTTPS request to the chatbot endpoint (as client)
 print("Sending requests to chatbot endpoint...")
 responses = []
-for q in input_questions:
+for i, q in enumerate(input_questions):
   try:
     r = requests.post(params={"api_key": key, "threadId": str(uuid.uuid4()), "include_context": True}, url=endpoint, json={"message": q})
   except requests.RequestException as e:
@@ -28,22 +28,25 @@ for q in input_questions:
     print(f"Error: Received status code {r.status_code} for question: {q}")
     continue
   response_dict = r.json()
-  responses.append({'user_input' : q,
+  responses.append({
+    'user_input' : q,
     'bot_output': response_dict['response'],
-    'context': json.dumps(response_dict['context'])}) # Convert context to JSON string for easier handling
+    'context': json.dumps(response_dict['context']),
+    'orig_idx' : i})
 print("Data generation completed.")
 
 # OUTPUT AS DATAFRAME
 results = []
 for i in range(len(responses)):
+    idx = responses[i]['orig_idx']
     results.append({
         "user_input": responses[i]['user_input'],
-        "expected_output": data['expected_output'][i],
+        "expected_output": data['expected_output'][idx],
         "bot_output": responses[i]['bot_output'],
         "context": responses[i]['context']
 
     })
 
 results_df = pd.DataFrame(results)
-results_df.to_csv( "../synthetic_data/goldens_final.csv", index=False, encoding='utf-8-sig')
+results_df.to_csv( "../data/goldens_final.csv", index=False, encoding='utf-8-sig')
 print("Questions, responses, and contexts saved.")
